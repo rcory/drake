8e7388f4b22c373252233e8f5f0b52ff193d3c08
hongkai-dai <daihongkai@gmail.com>
2017-08-19 13:44:39 -0700

    WIP: work on iiwa reachability through global ik

---
+++ drake/multibody/dev/test/iiwa_reachability_test.cc
@@ -0,0 +1,18 @@
+#include "drake/multibody/dev/test/global_inverse_kinematics_test_util.h"
+#include "drake/solvers/gurobi_solver.h"
+
+using Eigen::Vector3d;
+using Eigen::Isometry3d;
+
+using drake::solvers::SolutionResult;
+
+namespace drake {
+namespace multibody {
+namespace {
+TEST_F(KukaTest, Reachability) {
+// First generate the sample end effector position
+
+}
+}
+}
+}
