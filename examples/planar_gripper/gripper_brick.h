2d7837dac536a2eec71737a0b1e0d87b6da6d1d0
hongkai-dai <daihongkai@gmail.com>
2019-05-28 16:48:59 -0700

    Add GripperBrickSystem

---
+++ examples/planar_gripper/gripper_brick.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "drake/geometry/scene_graph.h"
+#include "drake/multibody/plant/multibody_plant.h"
+#include "drake/systems/framework/diagram.h"
+
+namespace drake {
+namespace examples {
+template <typename T>
+class GripperBrickSystem {
+ public:
+  GripperBrickSystem();
+
+  const systems::Diagram<T>& diagram() const { return *diagram_; }
+
+  systems::Diagram<T>* get_mutable_diagram() { return diagram_.get(); }
+
+  const multibody::MultibodyPlant<T>& plant() const { return *plant_; }
+
+  multibody::MultibodyPlant<T>* get_mutable_plant() { return plant_; }
+
+ private:
+  std::unique_ptr<systems::Diagram<T>> diagram_;
+  multibody::MultibodyPlant<T>* plant_;
+  geometry::SceneGraph<T>* scene_graph_;
+};
+
+}  // namespace examples
+}  // namespace drake
