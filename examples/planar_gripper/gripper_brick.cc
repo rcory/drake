ff2eeb2e2b25acd9c423555d69c6ddd0402fb0d5
hongkai-dai <daihongkai@gmail.com>
2019-05-29 17:02:08 -0700

    WIP: generate motions on the planar gripper.

--- examples/planar_gripper/gripper_brick.cc
+++ examples/planar_gripper/gripper_brick.cc
@@ -1,11 +1,36 @@
 #include "drake/examples/planar_gripper/gripper_brick.h"
 
 #include "drake/common/find_resource.h"
+#include "drake/geometry/geometry_visualization.h"
 #include "drake/multibody/parsing/parser.h"
+#include "drake/systems/analysis/simulator.h"
 #include "drake/systems/framework/diagram_builder.h"
 
 namespace drake {
 namespace examples {
+
+template <typename T>
+void AddDrakeVisualizer(systems::DiagramBuilder<T>*,
+                        const geometry::SceneGraph<T>&) {
+  // Disabling visualization for non-double scalar type T.
+}
+
+template <>
+void AddDrakeVisualizer<double>(
+    systems::DiagramBuilder<double>* builder,
+    const geometry::SceneGraph<double>& scene_graph) {
+  geometry::ConnectDrakeVisualizer(builder, scene_graph);
+}
+
+template <typename T>
+void InitializeDiagramSimulator(const systems::Diagram<T>&) {}
+
+template <>
+void InitializeDiagramSimulator<double>(
+    const systems::Diagram<double>& diagram) {
+  systems::Simulator<double>(diagram).Initialize();
+}
+
 template <typename T>
 void WeldGripperFrames(multibody::MultibodyPlant<T>* plant) {
   // This function is copied and adapted from planar_gripper_simulation.py
@@ -34,7 +59,7 @@ void WeldGripperFrames(multibody::MultibodyPlant<T>* plant) {
   // Weld the 3rd finger.
   const math::RigidTransformd X_PC3 =
       math::RigidTransformd(math::RollPitchYawd(M_PI / 3 * 2, 0, 0),
-                           Eigen::Vector3d::Zero()) *
+                            Eigen::Vector3d::Zero()) *
       X_PC2;
   const multibody::Frame<T>& finger3_base_frame =
       plant->GetFrameByName("finger3_base");
@@ -43,7 +68,7 @@ void WeldGripperFrames(multibody::MultibodyPlant<T>* plant) {
 
 template <typename T>
 std::unique_ptr<systems::Diagram<T>> ConstructDiagram(
-    multibody::MultibodyPlant<T>** plant,
+    bool add_gravity, multibody::MultibodyPlant<T>** plant,
     geometry::SceneGraph<T>** scene_graph) {
   systems::DiagramBuilder<T> builder;
   std::tie(*plant, *scene_graph) =
@@ -54,16 +79,25 @@ std::unique_ptr<systems::Diagram<T>> ConstructDiagram(
   parser.AddModelFromFile(gripper_path, "gripper");
   WeldGripperFrames(*plant);
   const std::string brick_path =
-      FindResourceOrThrow("drake/examples/planar_gripper/1dof_brick.sdf");
+      FindResourceOrThrow("drake/examples/planar_gripper/planar_brick.sdf");
   parser.AddModelFromFile(brick_path, "brick");
+  (*plant)->WeldFrames((*plant)->world_frame(),
+                       (*plant)->GetFrameByName("brick_base"),
+                       math::RigidTransformd());
+
+  if (add_gravity) {
+  }
 
   (*plant)->Finalize();
+
+  AddDrakeVisualizer<T>(&builder, **scene_graph);
   return builder.Build();
 }
 
 template <typename T>
-GripperBrickSystem<T>::GripperBrickSystem() {
-  diagram_ = ConstructDiagram<T>(&plant_, &scene_graph_);
+GripperBrickSystem<T>::GripperBrickSystem(bool add_gravity) {
+  diagram_ = ConstructDiagram<T>(add_gravity, &plant_, &scene_graph_);
+  InitializeDiagramSimulator(*diagram_);
 }
 
 // Explicit instantiation
