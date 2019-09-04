59987738352c47083dffd43c246eeec16c13fcdf
hongkai-dai <daihongkai@gmail.com>
2019-09-04 16:50:46 -0700

    modify the planar finger QP.

--- examples/planar_gripper/planar_finger_qp.cc
+++ examples/planar_gripper/planar_finger_qp.cc
@@ -10,19 +10,15 @@ const double kInf = std::numeric_limits<double>::infinity();
 PlanarFingerInstantaneousQP::PlanarFingerInstantaneousQP(
     const multibody::MultibodyPlant<double>* finger_brick, double theta_planned,
     double thetadot_planned, double thetaddot_planned, double Kp, double Kd,
-    const systems::Context<double>& plant_context,
+    double theta, double thetadot,
+    const Eigen::Ref<const Eigen::Vector2d>& p_BFingerTip,
     double weight_thetaddot_error, double weight_f_Cb, BrickFace contact_face,
-    double mu, const Eigen::Vector3d& p_L2FingerTip, double I_B,
-    double finger_tip_radius)
+    double mu, double I_B, double finger_tip_radius)
     : plant_{finger_brick},
       prog_{new solvers::MathematicalProgram()},
       f_Cb_B_edges_{prog_->NewContinuousVariables<2>()} {
   prog_->AddBoundingBoxConstraint(0, kInf, f_Cb_B_edges_);
-  Eigen::Vector3d p_BFingerTip;
-  plant_->CalcPointsPositions(
-      plant_context, plant_->GetFrameByName("finger_link2"), p_L2FingerTip,
-      plant_->GetFrameByName("brick_base_link"), &p_BFingerTip);
-  Eigen::Vector2d p_BCb = p_BFingerTip.tail<2>();
+  Eigen::Vector2d p_BCb = p_BFingerTip;
   switch (contact_face) {
     case BrickFace::kPosZ: {
       friction_cone_edges_ << -mu, mu, -1, -1;
@@ -51,12 +47,6 @@ PlanarFingerInstantaneousQP::PlanarFingerInstantaneousQP(
   const symbolic::Expression thetaddot =
       (p_BCb(0) * f_Cb_B(1) - p_BCb(1) * f_Cb_B(0)) / I_B;
 
-  const Eigen::Vector3d q = plant_->GetPositions(plant_context);
-  const Eigen::Vector3d v = plant_->GetVelocities(plant_context);
-  const int brick_theta_index =
-      plant_->GetJointByName("brick_pin_joint").position_start();
-  const double theta = q(brick_theta_index);
-  const double thetadot = v(brick_theta_index);
   const double thetaddot_des = Kp * (theta_planned - theta) +
                                Kd * (thetadot_planned - thetadot) +
                                thetaddot_planned;
