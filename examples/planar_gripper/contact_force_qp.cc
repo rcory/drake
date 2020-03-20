572fb02e2756492dbc56caee7cb22cec72fc07c9
Rick Cory <rick.cory@tri.global>
2020-03-20 15:37:03 -0700

    Fixes various vector sizes (previously hard coded) (#26)
    * Fixes various vector sizes (previously hard coded) to support both pin brick and planar brick spatial force planning.
    Also some documentation cleanup.

--- examples/planar_gripper/contact_force_qp.cc
+++ examples/planar_gripper/contact_force_qp.cc
@@ -34,25 +34,53 @@ Eigen::Matrix2d GetFrictionConeEdges(double mu, BrickFace brick_face) {
 }
 
 InstantaneousContactForceQP::InstantaneousContactForceQP(
-    const BrickType brick_type,
-    const Eigen::Ref<const Eigen::Vector2d>& p_WB_planned,
-    const Eigen::Ref<const Eigen::Vector2d>& v_WB_planned,
-    const Eigen::Ref<const Eigen::Vector2d>& a_WB_planned, double theta_planned,
-    double thetadot_planned, double thetaddot_planned,
+    const BrickType& brick_type,
+    const Eigen::Ref<const VectorX<double>>& brick_state,
+    const Eigen::Ref<const VectorX<double>>& brick_state_desired,
+    const Eigen::Ref<const VectorX<double>>& brick_accel_feedforward,
     const Eigen::Ref<const Eigen::Matrix2d>& Kp_t,
     const Eigen::Ref<const Eigen::Matrix2d>& Kd_t, double Kp_r, double Kd_r,
-    const Eigen::Ref<const Vector6<double>>& brick_state,
     const std::unordered_map<Finger, std::pair<BrickFace, Eigen::Vector2d>>&
         finger_face_assignments,
     double weight_a_error, double weight_thetaddot_error, double weight_f_Cb,
     double mu, double I_B, double brick_mass, double rotational_damping,
     double translational_damping)
     : prog_{new solvers::MathematicalProgram()} {
-  // Extract the brick state.
-  const Eigen::Vector2d p_WB = brick_state.head<2>();
-  const Eigen::Vector2d v_WB = brick_state.segment<2>(3);
-  const double theta = brick_state(2);
-  const double thetadot = brick_state(5);
+  Eigen::Vector2d p_WB = Eigen::Vector2d::Zero();
+  Eigen::Vector2d p_WB_desired = Eigen::Vector2d::Zero();
+  Eigen::Vector2d v_WB = Eigen::Vector2d::Zero();
+  Eigen::Vector2d v_WB_desired = Eigen::Vector2d::Zero();
+  Eigen::Vector2d a_WB_feedforward = Eigen::Vector2d::Zero();
+  double theta, thetadot;
+  double theta_desired, thetadot_desired, thetaddot_feedforward;
+
+  if (brick_type == BrickType::PinBrick) {
+    // Extract the pined brick state: given as [θ, θ̇̇ ].
+    theta = brick_state(0);
+    thetadot = brick_state(1);
+
+    // Extract the desired values.
+    theta_desired = brick_state_desired(0);
+    thetadot_desired = brick_state_desired(1);
+    thetaddot_feedforward = brick_accel_feedforward(0);
+  } else if (brick_type == BrickType::PlanarBrick) {
+    // Extract the planar brick position and velocity values from the incoming
+    // planar brick state vector, given as [y, z, θ, ẏ, ż, θ̇ ̇̇]ᵀ
+    p_WB = brick_state.head<2>();
+    v_WB = brick_state.segment<2>(3);
+    theta = brick_state(2);
+    thetadot = brick_state(5);
+
+    // Extract the desired values.
+    p_WB_desired = brick_state_desired.head<2>();
+    v_WB_desired = brick_state_desired.segment<2>(3);
+    theta_desired = brick_state_desired(2);
+    thetadot_desired = brick_state_desired(5);
+    a_WB_feedforward = brick_accel_feedforward.head<2>();
+    thetaddot_feedforward = brick_accel_feedforward(2);
+  } else {
+    throw std::logic_error("Unknown BrickType.");
+  }
 
   std::map<Finger, Vector2<symbolic::Expression>> f_Cb_B;
   // Also compute the total contact torque.
@@ -79,40 +107,40 @@ InstantaneousContactForceQP::InstantaneousContactForceQP(
                                        f_Cbi_B_edges, mu, p_BCbi);
   }
 
-  // Add the damping term
+  // Add the rotational damping term
   total_torque += -rotational_damping * thetadot;
 
-  // First compute the desired acceleration.
-  const Eigen::Vector2d a_WB_des = Kp_t * (p_WB_planned - p_WB) +
-                                   Kd_t * (v_WB_planned - v_WB) + a_WB_planned;
-  const double thetaddot_des = Kp_r * (theta_planned - theta) +
-                               Kd_r * (thetadot_planned - thetadot) +
-                               thetaddot_planned;
-
-  const double cos_theta = std::cos(theta);
-  const double sin_theta = std::sin(theta);
-  Eigen::Matrix2d R_WB;
-  R_WB << cos_theta, -sin_theta, sin_theta, cos_theta;
-  // Total contact force in the brick frame.
-  Vector2<symbolic::Expression> f_total_B(0, 0);
-  for (const auto& face_contact_force : f_Cb_B) {
-    f_total_B += face_contact_force.second;
-  }
-
-  Eigen::Matrix2d D;
-  D << translational_damping, 0, 0, translational_damping;
-  Vector2<symbolic::Expression> a_WB =
-      Eigen::Vector2d(0, 0 * -multibody::UniformGravityFieldElement<
-                                 double>::kDefaultStrength) +
-      (R_WB * f_total_B - D * brick_state.segment<2>(3)) / brick_mass;
-
+  // Form the desired angular acceleration as a PD + FF term.
+  const double thetaddot_des = Kp_r * (theta_desired - theta) +
+                               Kd_r * (thetadot_desired - thetadot) +
+                               thetaddot_feedforward;
   symbolic::Expression thetaddot = total_torque / I_B;
-  if (brick_type == BrickType::PlanarBrick) {
-    prog_->AddQuadraticCost((a_WB - a_WB_des).squaredNorm() * weight_a_error);
-  }
-  using std::pow;
   prog_->AddQuadraticCost(pow(thetaddot - thetaddot_des, 2) *
                           weight_thetaddot_error);
+
+  // If applicable, add cost on linear acceleration.
+  if (brick_type == BrickType::PlanarBrick) {
+    const double cos_theta = std::cos(theta);
+    const double sin_theta = std::sin(theta);
+    Eigen::Matrix2d R_WB;
+    R_WB << cos_theta, -sin_theta, sin_theta, cos_theta;
+    // Total contact force in the brick frame.
+    Vector2<symbolic::Expression> f_total_B(0, 0);
+    for (const auto& face_contact_force : f_Cb_B) {
+      f_total_B += face_contact_force.second;
+    }
+    Eigen::Matrix2d D;
+    D << translational_damping, 0, 0, translational_damping;
+    Vector2<symbolic::Expression> a_WB =
+        Eigen::Vector2d(0, 0 * -multibody::UniformGravityFieldElement<
+            double>::kDefaultStrength) +
+            (R_WB * f_total_B - D * v_WB) / brick_mass;
+    const Eigen::Vector2d a_WB_des = Kp_t * (p_WB_desired - p_WB) +
+        Kd_t * (v_WB_desired - v_WB) + a_WB_feedforward;
+    prog_->AddQuadraticCost((a_WB - a_WB_des).squaredNorm() * weight_a_error);
+  }
+
+  // Adds a cost on the squared norm of the forces.
   for (const auto& finger_contact_force : f_Cb_B) {
     prog_->AddQuadraticCost(finger_contact_force.second.squaredNorm() *
                             weight_f_Cb);
@@ -141,18 +169,18 @@ InstantaneousContactForceQP::GetContactForceResult(
 
 InstantaneousContactForceQPController::InstantaneousContactForceQPController(
     const BrickType brick_type, const multibody::MultibodyPlant<double>* plant,
-    const Eigen::Ref<const Eigen::Matrix2d>& Kp_tr,
-    const Eigen::Ref<const Eigen::Matrix2d>& Kd_tr, double Kp_ro, double Kd_ro,
+    const Eigen::Ref<const Eigen::Matrix2d>& Kp_t,
+    const Eigen::Ref<const Eigen::Matrix2d>& Kd_t, double Kp_r, double Kd_r,
     double weight_a_error, double weight_thetaddot_error, double weight_f_Cb_B,
     double mu, double translational_damping, double rotational_damping,
     double I_B, double mass_B)
     : brick_type_(brick_type),
       plant_{plant},
       mu_{mu},
-      Kp_tr_{Kp_tr},
-      Kd_tr_{Kd_tr},
-      Kp_ro_{Kp_ro},
-      Kd_ro_{Kd_ro},
+      Kp_t_{Kp_t},
+      Kd_t_{Kd_t},
+      Kp_r_{Kp_r},
+      Kd_r_{Kd_r},
       weight_a_error_{weight_a_error},
       weight_thetaddot_error_{weight_thetaddot_error},
       weight_f_Cb_B_{weight_f_Cb_B},
@@ -161,8 +189,8 @@ InstantaneousContactForceQPController::InstantaneousContactForceQPController(
       mass_B_(mass_B),
       I_B_(I_B) {
   // TODO(rcory) Check positive definiteness of Kp_tr and Kd_tr
-  DRAKE_DEMAND(Kp_ro_ >= 0);
-  DRAKE_DEMAND(Kd_ro_ >= 0);
+  DRAKE_DEMAND(Kp_r_ >= 0);
+  DRAKE_DEMAND(Kd_r_ >= 0);
   DRAKE_DEMAND(weight_a_error_ >= 0);
   DRAKE_DEMAND(weight_thetaddot_error_ >= 0);
   DRAKE_DEMAND(weight_f_Cb_B_ >= 0);
@@ -203,22 +231,21 @@ InstantaneousContactForceQPController::InstantaneousContactForceQPController(
           .get_index();
 
   if (brick_type == BrickType::PlanarBrick) {
-    input_index_desired_brick_state_ =  // {y, z, theta, ydot, zdot, thetadot}
+    input_index_desired_brick_state_ =
         this->DeclareInputPort("desired_brick_state", systems::kVectorValued, 6)
             .get_index();
-    input_index_desired_brick_acceleration_ =  // {yddot, zddot, thetaddot}
+    input_index_desired_brick_acceleration_ =
         this->DeclareInputPort("desired_brick_accel", systems::kVectorValued, 3)
-            .get_index();  // {ay, az, w_x}
-
+            .get_index();
     brick_translate_y_position_index_ =
         plant_->GetJointByName("brick_translate_y_joint").position_start();
     brick_translate_z_position_index_ =
         plant_->GetJointByName("brick_translate_z_joint").position_start();
-  } else {                              // Pin Brick
-    input_index_desired_brick_state_ =  // {theta, thetadot}
+  } else {  // Pin Brick
+    input_index_desired_brick_state_ =
         this->DeclareInputPort("desired_brick_state", systems::kVectorValued, 2)
             .get_index();
-    input_index_desired_brick_acceleration_ =  // thetaddot
+    input_index_desired_brick_acceleration_ =
         this->DeclareInputPort("desired_brick_accel", systems::kVectorValued, 1)
             .get_index();
   }
@@ -256,24 +283,9 @@ void InstantaneousContactForceQPController::CalcFingersControl(
     return;
   }
 
-  Eigen::Vector2d p_WB_planned;
-  Eigen::Vector2d v_WB_planned;
-  Eigen::Vector2d a_WB_planned;
-  double theta_planned;
-  double thetadot_planned;
-  double thetaddot_planned;
-  Vector6<double> brick_state;
-
-  // TODO(rcory) Use properly defined indices to extract the desired values,
-  //  instead of hard-coding them below.
+  VectorX<double> brick_state;
   if (brick_type_ == BrickType::PlanarBrick) {
-    p_WB_planned = desired_brick_state.head<2>();
-    v_WB_planned = desired_brick_state.segment<2>(3);
-    a_WB_planned = desired_brick_acceleration.head<2>();
-    theta_planned = desired_brick_state(2);
-    thetadot_planned = desired_brick_state(5);
-    thetaddot_planned = desired_brick_acceleration(2);
-
+    brick_state = Vector6<double>::Zero();
     brick_state << plant_state(brick_translate_y_position_index_),
         plant_state(brick_translate_z_position_index_),
         plant_state(brick_revolute_x_position_index_),
@@ -283,28 +295,21 @@ void InstantaneousContactForceQPController::CalcFingersControl(
                     brick_translate_z_position_index_),
         plant_state(plant_->num_positions() + brick_revolute_x_position_index_);
   } else {  // brick_type is PinBrick
-    p_WB_planned = Eigen::Vector2d::Zero();
-    v_WB_planned = Eigen::Vector2d::Zero();
-    a_WB_planned = Eigen::Vector2d::Zero();
-    theta_planned = desired_brick_state(0);
-    thetadot_planned = desired_brick_state(1);
-    thetaddot_planned = desired_brick_acceleration(0);
-
-    brick_state << 0, 0, plant_state(brick_revolute_x_position_index_), 0, 0,
+    brick_state = Eigen::Vector2d::Zero();
+    brick_state << plant_state(brick_revolute_x_position_index_),
         plant_state(plant_->num_positions() + brick_revolute_x_position_index_);
   }
 
   InstantaneousContactForceQP qp(
-      brick_type_, p_WB_planned, v_WB_planned, a_WB_planned, theta_planned,
-      thetadot_planned, thetaddot_planned, Kp_tr_, Kd_tr_, Kp_ro_, Kd_ro_,
-      brick_state, finger_face_assignments, weight_a_error_,
+      brick_type_, brick_state, desired_brick_state, desired_brick_acceleration,
+      Kp_t_, Kd_t_, Kp_r_, Kd_r_, finger_face_assignments, weight_a_error_,
       weight_thetaddot_error_, weight_f_Cb_B_, mu_, I_B_, mass_B_,
       rotational_damping_, translational_damping_);
 
   const auto qp_result = solvers::Solve(qp.prog());
   const std::unordered_map<Finger, std::pair<Eigen::Vector2d, Eigen::Vector2d>>
       finger_contact_result = qp.GetContactForceResult(qp_result);
-  const double theta = brick_state(2);
+  const double theta = plant_state(brick_revolute_x_position_index_);
   const double cos_theta = std::cos(theta);
   const double sin_theta = std::sin(theta);
   Eigen::Matrix2d R_WB;
@@ -314,8 +319,6 @@ void InstantaneousContactForceQPController::CalcFingersControl(
     spatial_force.body_index = brick_body_index_;
     const std::pair<Eigen::Vector2d, Eigen::Vector2d>& force_position =
         finger_contact_result.at(finger_face_assignment.first);
-    //    drake::log()->info("force: \n{}", force_position.first);
-    //    drake::log()->info("position: \n{}", force_position.second);
     const Eigen::Vector2d p_BCb = force_position.second;
     spatial_force.p_BoBq_B = Eigen::Vector3d(0, p_BCb(0), p_BCb(1));
     const Eigen::Vector2d f_Cb_W = R_WB * force_position.first;
--- examples/planar_gripper/contact_force_qp.h
+++ examples/planar_gripper/contact_force_qp.h
@@ -25,37 +25,46 @@ enum class BrickType {
 };
 
 /**
- * Solves a QP to find the contact force from the finger to the object, such
+ * Solves a QP to find the spatial force to be applied on the object, such
  * that the object is steered back to the desired trajectory. Note this only
- * finds the contact force in the next dt.
+ * finds the contact force in the next dt (i.e., solves a one-step QP).
  * Mathematically, we solve the following quadratic programming problem
  *
  *     min   w₁ * |p̈_WB - p̈ᵈᵉˢ_WB|² + w₂  * |θ̈ - θ̈ᵈᵉˢ|² + w₃ * |f_Cb_B|²
- *     s.t   m * p̈_WB = m * g_W + R_WB * ∑ f_Cb_B
- *           I * θ̈ = ∑ p_Cb_B * f_Cb_B
- *           f_Cb_B in the friction cone.
- * where p̈ᵈᵉˢ_WB = Kp1 * (p_planned_WB - p_WB) + Kd1 * (ṗ_planned_WB - ṗ_WB) +
- * p̈_planned_WB, and θ̈ᵈᵉˢ = Kp2 * (θ_planned - θ) + Kd2 * (θ̇_planned - θ̇ ) + θ̈
- * _planned.
+ *     s.t   m * p̈_WB = m * g_W + R_WB * ∑ f_Cb_B - (D_t * ṗ_WB)
+ *           I * θ̈ = ∑ p_Cb_B * f_Cb_B - (D_r * θ̇)
+ *           f_Cb_B ∈ friction cone.
+ * where
+ * p̈ᵈᵉˢ_WB = Kp1 * (p_desired_WB - p_WB) + Kd1 * (ṗ_desired_WB - ṗ_WB) +
+ * p̈_feedforward_WB, and
+ * θ̈ᵈᵉˢ = Kp2 * (θ_desired - θ) + Kd2 * (θ̇_desired - θ̇ ) + θ̈_feedforward.
+ * D_t and D_r are translational and rotational viscous damping coefficients,
+ * e.g., as provided by the joint `<damping>` tag in `sdf`.
  */
+// TODO(rcory) Include stiction and coulomb friction terms in the dynamic
+//  constraints above (see issue #15).
 class InstantaneousContactForceQP {
  public:
   /**
-   * @param gripper_brick The helper class. Must remain active during the
-   * lifetime of this QP.
-   * @param p_WB_planned The planned position of the brick in the world frame.
-   * @param v_WB_planned The planned velocity of the brick in the world frame.
-   * @param a_WB_planned The planned acceleration of the brick in the worl
-   * frame.
-   * @param theta_planned The planned orientation of the brick.
-   * @param thetadot_planned The planned angular velocity of the brick.
-   * @param thetaddot_planned The planned angular acceleration of the brick.
+   * @param brick_type The type of brick. Can be either BrickType::PlanarBrick
+   * or BrickType::PinBrick.
+   * @param brick_state The current state of the brick. If brick_type is
+   * BrickType::PlanarBrick, the state vector is: [y, z, θ, ẏ, ż, θ̇]ᵀ,,
+   * namely the planar brick position/orientation and its velocities, in this
+   * order. If brick_type is BrickType::PinBrick, the state vector is [θ, θ̇]ᵀ,
+   * @param brick_state_desired The desired state of the brick. If brick_type is
+   * BrickType::PlanarBrick, the desired state vector is: [y, z, θ, ẏ, ż, θ̇]ᵀ,
+   * namely the planar brick position/orientation and its velocities, in this
+   * order. If brick_type is BrickType::PinBrick, the desired state vector is
+   * [θ, θ̇]ᵀ,
+   * @param brick_accel_feedforward The feedforward acceleration of the brick.
+   * If brick_type is BrickType::PlanarBrick, the feedforward acceleration
+   * vector is: [ÿ, z̈, θ̈]ᵀ. If brick_type is BrickType::PinBrick, the
+   * feedforward acceleration is a 1-dimensional vector θ̈.
    * @param Kp_t The proportional gain of the brick translational position.
    * @param Kd_t The derivative gain of the brick translational velocity.
    * @param Kp_r The proportional gain of the brick rotational angle.
    * @param Kd_r The derivative gain of the brick rotational (angular) velocity.
-   * @param brick_state The brick state include [p_y, p_z, θ, ṗ_y, ṗ_z, θ̇],
-   * namely the brick position/orientation and its velocities.
    * @param finger_face_assignments a std::unordered_map which assigns a Finger
    * to a contact face on the brick (BrickFace) and to a contact point on the
    * brick. finger_face_assignments[Finger] gives a std::pair(BrickFace,
@@ -63,9 +72,9 @@ class InstantaneousContactForceQP {
    * measured from the brick's body origin Bo, expressed in the brick's body
    * frame B.
    * @param weight_a_error The weight of the acceleration error in the cost.
-   * This error is made up of a PD term and a FF (planned) term.
+   * This error is made up of a PD term and a FF term.
    * @param weight_thetaddot_error The weight of the angular acceleration error
-   * in the cost. This error is made up of a PD term and a FF (planned) term.
+   * in the cost. This error is made up of a PD term and a FF term.
    * @param weight_f_Cb The weight of the contact force in the cost.
    * @param mu The brick/floor coefficient of static friction (stiction).
    * @param I_B The brick's rotational moment of inertia around its axis of
@@ -75,14 +84,12 @@ class InstantaneousContactForceQP {
    * @param translational_damping The translational viscous damping coefficient.
    */
   InstantaneousContactForceQP(
-      const BrickType brick_type,
-      const Eigen::Ref<const Eigen::Vector2d>& p_WB_planned,
-      const Eigen::Ref<const Eigen::Vector2d>& v_WB_planned,
-      const Eigen::Ref<const Eigen::Vector2d>& a_WB_planned,
-      double theta_planned, double thetadot_planned, double thetaddot_planned,
+      const BrickType& brick_type,
+      const Eigen::Ref<const VectorX<double>>& brick_state,
+      const Eigen::Ref<const VectorX<double>>& brick_state_desired,
+      const Eigen::Ref<const VectorX<double>>& brick_accel_feedforward,
       const Eigen::Ref<const Eigen::Matrix2d>& Kp_t,
       const Eigen::Ref<const Eigen::Matrix2d>& Kd_t, double Kp_r, double Kd_r,
-      const Eigen::Ref<const Vector6<double>>& brick_state,
       const std::unordered_map<Finger, std::pair<BrickFace, Eigen::Vector2d>>&
           finger_face_assignments,
       double weight_a_error, double weight_thetaddot_error, double weight_f_Cb,
@@ -128,16 +135,16 @@ class InstantaneousContactForceQPController
 
   /**
    * @param gripper_brick The gripper_brick system.
-   * @param Kp_tr The proportional gain for the brick y/z translational
+   * @param Kp_t The proportional gain for the brick y/z translational
    * position.
-   * @param Kd_tr The derivative gain for the brick y/z translational position.
-   * @param Kp_ro The proportional gain for the brick rotational position
+   * @param Kd_t The derivative gain for the brick y/z translational position.
+   * @param Kp_r The proportional gain for the brick rotational position
    * (angle).
-   * @param Kd_ro The derivative gain for the brick rotational velocity.
+   * @param Kd_r The derivative gain for the brick rotational velocity.
    * @param weight_a_error The weighting for the brick y/z acceleration in the
-   * cost. This error is made up of a PD term and a FF (planned) term.
+   * cost. This error is made up of a PD term and a FF term.
    * @param weight_thetaddot_error The weighting for the brick thetaddot in the
-   * cost. This error is made up of a PD term and a FF (planned) term.
+   * cost. This error is made up of a PD term and a FF term.
    * @param weight_f_Cb_B The weighting for the contact forces in the cost.
    * @param mu The friction coefficient used to compute friction cones.
    *        Note: This value is strictly used for modeling the frictional force
@@ -240,10 +247,10 @@ class InstantaneousContactForceQPController
   const BrickType brick_type_;
   const multibody::MultibodyPlant<double>* plant_;
   double mu_;
-  Eigen::Matrix2d Kp_tr_;  // Translational proportional QP gain.
-  Eigen::Matrix2d Kd_tr_;  // Translational derivative QP gain.
-  double Kp_ro_;  // Rotational proportional QP gain.
-  double Kd_ro_;  // Rotational derivative QP gain.
+  Eigen::Matrix2d Kp_t_;  // Translational proportional QP gain.
+  Eigen::Matrix2d Kd_t_;  // Translational derivative QP gain.
+  double Kp_r_;  // Rotational proportional QP gain.
+  double Kd_r_;  // Rotational derivative QP gain.
   double weight_a_error_;
   double weight_thetaddot_error_;
   double weight_f_Cb_B_;
